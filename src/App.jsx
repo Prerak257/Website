1cd3c640ac236bd2728402a1dece52e47f4e46b1
pujanjoci <pujanjoci2@gmail.com>
2025-09-15 16:44:32 +0545

    updated code for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { useState, useEffect, Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
 import Header from './components/Header'
